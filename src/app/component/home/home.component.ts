82981c2e070de79d1f3eb3232bbe76a3b33af892
dmuhila <925dmuhila@gmail.com>
2021-07-30 11:22:38 +0100

    css change in chart

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ChartReadyEvent, GoogleChartInterface } from 'ng2-google-charts';
 import { CovidDataService } from 'src/app/covid-data.service';
 import { CovidData } from 'src/app/models/covid-data';
@@ -11,25 +11,25 @@ import { CovidData } from 'src/app/models/covid-data';
 export class HomeComponent implements OnInit {
   pieChart: GoogleChartInterface;
   columnChart: GoogleChartInterface;
-  @ViewChild('mychart', {static: false}) mychart;
-  @ViewChild('mychart1', {static: false}) mychart1;
+  @ViewChild('mychart', { static: false }) mychart;
+  @ViewChild('mychart1', { static: false }) mychart1;
 
 
   totalConfirmed = 0;
   totalRecovered = 0;
   totalDeath = 0;
   totalActive = 0;
-  globalData : CovidData[];
-  isData : boolean = false;
-  category : string ;
+  globalData: CovidData[];
+  isData: boolean = false;
+  category: string;
 
-  constructor(private report:CovidDataService) { }
+  constructor(private report: CovidDataService) { }
 
   ngOnInit(): void {
-    this.report.getData().subscribe((data: any)=>{
+    this.report.getData().subscribe((data: any) => {
       //console.log(data);
-      this.globalData = [...data]; 
-      data.forEach((d)=>{
+      this.globalData = [...data];
+      data.forEach((d) => {
         this.totalConfirmed += d.confirmed;
         this.totalRecovered += d.recovered;
         this.totalDeath += d.death;
@@ -41,34 +41,33 @@ export class HomeComponent implements OnInit {
     })
   }
 
-  initChart(category:string, refresh:boolean){
+  initChart(category: string, refresh: boolean) {
     var dataTable = [];
     //console.log("category",category);
-       
+
     dataTable.push(['Countries', 'Cases']);
-    this.globalData?.forEach((d)=>{
-      let value : number;
-      let country : string;
+    this.globalData?.forEach((d) => {
+      let value: number;
+      let country: string;
       //console.log("in forEach",category);
-      if(category=='confirmed' && d?.confirmed > 1000000)
-      {
+      if (category == 'confirmed' && d?.confirmed > 1000000) {
         //console.log("in con");    
         value = d.confirmed;
         country = d.country;
-        
+
       }
-      else if(category=='recovered' && d?.recovered > 1000){
+      else if (category == 'recovered' && d?.recovered > 1000) {
         value = d.recovered;
         country = d.country;
       }
-      else if(category=='active' && d?.active > 1000){
+      else if (category == 'active' && d?.active > 1000) {
         value = d.active;
         country = d.country;
       }
-      if(country)
-      dataTable.push([country, value]);   
+      if (country)
+        dataTable.push([country, value]);
     })
-    
+
 
     //console.log(dataTable);
 
@@ -78,32 +77,33 @@ export class HomeComponent implements OnInit {
       dataTable: dataTable,
       //firstRowIsData: true,
       options: {
-        height:500
+        height: 500,
+        width: 550
       },
     };
-    
+
     //columnChart
-    this.columnChart =  {
+    this.columnChart = {
       chartType: 'ColumnChart',
       dataTable: [...dataTable],
       options: {
-        height:500
+        height: 500,
+        width: 550
       }
     }
     this.isData = true;
     //console.log(this.mychart);
-    if(refresh)
-    {
-      setTimeout(() => this.mychart?.draw(),250);
-      setTimeout(() => this.mychart1?.draw(),250)
+    if (refresh) {
+      setTimeout(() => this.mychart?.draw(), 250);
+      setTimeout(() => this.mychart1?.draw(), 250)
 
     }
   }
 
-  onSelect(category){
+  onSelect(category) {
     //console.log("button",category);
-    this.initChart(category,true);
-    
+    this.initChart(category, true);
+
   }
 
   ready(event: ChartReadyEvent) {
